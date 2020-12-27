831c7479b89762e5d5b2b5ff3371a5ac650cf7d9
José Diaz <xoxefdp@gmail.com>
2020-12-28 00:21:21 +0100

    Mvoed wanip request logic to main process, commented all consoles output

--- src/main.js
+++ src/main.js
@@ -1,7 +1,62 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const os = require('os');
+const { app, BrowserWindow, ipcMain, net } = require('electron')
+const os = require('os')
 
 let mainWindow;
+
+function requestWanInterface() {
+  const request = net.request('https://bot.whatismyipaddress.com')
+  request.on('response', (response) => {
+    // console.log(`STATUS: ${response.statusCode}`)
+    // console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+    // console.log(`RESPONSE: ${JSON.stringify(response)}`)
+    response.on('data', (data) => {
+      // console.log(`BODY: ${data}`)
+      mainWindow.webContents.send("send-wan-ip", data);
+    })
+    response.on('error', (error) => {
+      // console.log(`ERROR response: ${JSON.stringify(error)}`)
+      mainWindow.webContents.send("send-wan-ip", null);
+    })
+    response.on('end', () => {
+      // console.log('No more data in response.')
+    })
+  })
+  request.on('error', (error) => {
+    // console.log(`ERROR request: ${JSON.stringify(error)}`)
+    mainWindow.webContents.send("send-wan-ip", null);
+  })
+  request.end()
+}
+
+function requestLocalInterfaces() {
+  const networkInterfaces = os.networkInterfaces()
+  let localInterfaces = []
+  Object.keys(networkInterfaces).forEach( (ifname) => {
+    let alias = 0;
+    networkInterfaces[ifname].forEach( (iface) => {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        return;
+      }
+      if (alias >= 1) {
+        localInterfaces.push({
+          name: ifname + ':' + alias,
+          address: iface.address
+        })
+        // console.log(ifname + ':' + alias, iface.address)
+      } else {
+        localInterfaces.push({
+          name: ifname,
+          address: iface.address
+        })
+        // console.log(ifname, iface.address)
+      }
+      ++alias
+    });
+  });
+
+  // console.log(localInterfaces)
+  mainWindow.webContents.send("send-lan-ip", localInterfaces);
+}
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -21,48 +76,22 @@ app.on('ready', () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
 
-
-function requestLocalInterfaces() {
-  const networkInterfaces = os.networkInterfaces()
-  let localInterfaces = []
-  Object.keys(networkInterfaces).forEach( (ifname) => {
-    let alias = 0;
-    networkInterfaces[ifname].forEach( (iface) => {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
-        return;
-      }
-      if (alias >= 1) {
-        localInterfaces.push({
-          name: ifname + ':' + alias,
-          address: iface.address
-        })
-        console.log(ifname + ':' + alias, iface.address)
-      } else {
-        localInterfaces.push({
-          name: ifname,
-          address: iface.address
-        })
-        console.log(ifname, iface.address)
-      }
-      ++alias
-    });
-  });
-
-  console.log(localInterfaces)
-  mainWindow.webContents.send("send-lan-ip", localInterfaces);
-}
-
+ipcMain.on('request-wan-ip', (event) => {
+  // console.log('request-wan-ip')
+  requestWanInterface()
+})
 
 ipcMain.on('request-lan-ip', (event) => {
-  console.log('request-lan-ip')
+  // console.log('request-lan-ip')
   requestLocalInterfaces()
 })
 
 ipcMain.on('network-status', (event, status) => {
-  console.log(status)
+  // console.log('network-status')
+  // console.log(status)
 })
 
 ipcMain.on('application-close', (event) => {
-  console.log('application-close')
+  // console.log('application-close')
   app.quit()
 })
